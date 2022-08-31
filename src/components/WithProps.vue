a530cab8b88b219dc85fc56f16ed20eeb3aa584f
Orian <griffith73000@gmail.com>
2022-08-31 16:13:21 +0200

    Training usingVue

---
+++ src/components/WithProps.vue
@@ -0,0 +1,21 @@
+<script>
+    export default{
+        props : {
+            text : {
+                type: String
+            }
+        },
+        methods:{
+            loadPage(page){
+                this.$parent.loadPage(page);
+            }
+        }
+    }
+
+</script>
+
+<template>
+
+<h2 class="w3-center">{{text}}</h2>
+            
+</template>
